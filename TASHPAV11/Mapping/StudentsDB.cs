a24364bc1b00fe59ba6aa212407a4d74fb4e2ee6
doronshafrir <doron.shafrir.74@gmail.com>
2025-10-24 09:36:13 +0300

    With the changes in SelectAll

--- TASHPAV11/Mapping/StudentsDB.cs
+++ TASHPAV11/Mapping/StudentsDB.cs
@@ -13,10 +13,12 @@ namespace TASHPAV11.Mapping
         public Studentss SelectAll()
         {
             Studentss students = new Studentss();
-            const string sql = "SELECT student_person.[Name] AS StudentName, " +
-               "c.[CourseName], teacher_person.Name AS TeacherName " +
+            Student student = new Student();
+            int count = 0;
+            const string sql = "SELECT student_person.[Id] AS StudentId, student_person.[Name] AS StudentName, " +
+               "c.[CourseName], teacher_person.Id AS TeacherId " +
                "FROM (([Student] " +
-               "INNER JOIN [Person] AS student_person ON [Student].[SId ] = student_person.[Id]) " +
+               "INNER JOIN [Person] AS student_person ON [Student].[SId] = student_person.[Id]) " +
                "INNER JOIN [Courses] AS c ON [Student].[CourseId] = c.[CId]) " +
                 " INNER JOIN [Person] AS teacher_person ON c.[ResponsibleTeacher] = teacher_person.[Id];";
 
@@ -26,15 +28,16 @@ namespace TASHPAV11.Mapping
             connection.Open();
 
             using var reader = command.ExecuteReader();
-
-            while (reader!.Read())
+            
+            while (reader.Read())
             {
-                Student student = new Student();
-                student = new Student
+                count++;
+               student = new Student()
                 {
+                    Id = (int)reader["StudentId"],
                     Name = reader["StudentName"].ToString(),
-                    CourseName = reader["StudentName"].ToString(),
-                    CourseNumber = reader["CourseNumber"].ToString(),
+                    CourseName = reader["CourseName"].ToString(),
+                    ResponsibleTeacher = (int)reader["TeacherId"]
 
                 };
                 students.Add(student);
@@ -43,22 +46,20 @@ namespace TASHPAV11.Mapping
         }
 
 
-        public int Insert(Course course)
+        public int Insert(int SId, int CourseId)
         {
             int records = 0;
-            string arg1 = course.CourseName;
-            string arg2 = course.CourseNumber;
-            int arg3 = CheckName(course.Name);
-            if (!(arg3 > 0)) return 0;
-
-            string sql = $"INSERT INTO Courses (CourseName, CourseNumber, ResponsibleTeacher) " +
-                $"VALUES('{arg1}','{arg2}', {arg3}) ";
+            int arg1 = SId;
+            int arg2 = CourseId;
+            if (arg1 != 0 && arg2 != 0)
+            { 
+            string sql = $"INSERT INTO Student VALUES('{arg1}','{arg2}')";
             using var connection = new OleDbConnection(connectionString);
             using var command = new OleDbCommand(sql, connection);
             connection.Open();
 
             records = (int)command.ExecuteNonQuery();
-
+            }
             return records;
         }
 
